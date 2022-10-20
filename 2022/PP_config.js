26296ee67b3bd6d7374be87f093571f682ee219b
Paul Nelson <116308944+f1stealth@users.noreply.github.com>
2022-10-20 15:50:27 -0400

    Add files via upload

---
+++ 2022/PP_config.js
@@ -0,0 +1,229 @@
+var config_data = `
+{
+  "title":"FTC Power Play Scouting",
+  "page_title":"FTC Power Play",
+  "elements":{
+    "prematch": {
+      "Scouter Initials": {
+        "code":"s",
+        "type":"scouter",
+        "size":5,
+        "maxSize":5,
+        "required":"true"
+      },
+      "Event":{
+        "code":"e",
+        "type":"event",
+        "defaultValue":"BedfordQualifer",
+        "required":"true",
+        "disabled":"true"
+      },
+      "Match Level":{
+        "code":"l",
+        "type":"level",
+        "choices":{
+          "qm":"Quals<br>",
+          "ef":"Eighth-Final<br>",
+          "qf":"Quarter-Final<br>",
+          "sf":"Semi-Final<br>",
+          "f":"Final"
+        },
+        "defaultValue":"qm",
+        "required":"true"
+      },
+      "Match #":{
+        "code":"m",
+        "type":"match",
+        "min":1,
+        "max":100,
+        "required":"true"
+      },
+     "Robot": {
+        "code":"r",
+        "type":"robot",
+        "choices":{
+          "r1":"Red-1",
+          "b1":"Blue-1<br>",
+          "r2":"Red-2",
+          "b2":"Blue-2<br>"
+        },
+        "required":"true"
+      },
+      "Team #": {
+        "code":"t",
+        "type":"team",
+        "min":1,
+        "max":99999
+      },
+      "Auto Start Position (required)": {
+        "code":"as",
+        "title": "Auto Start Position",
+        "type":"field_image",
+        "filename":"2022/field_image.png"
+      }
+    },
+    "auton": {
+      "Cones: In Terminal": {
+        "code":"autocit",
+        "title": "Cones: In Terminal",
+        "type":"counter"
+      },
+      "Cones: Ground Junction": {
+        "code":"autocgj",
+        "title": "Cones: Ground Junction",
+        "type":"counter"
+      },
+      "Cones: Low Junction": {
+        "code":"autoclj",
+        "title": "Cones: Low Junction",
+        "type":"counter"
+      },
+      "Cones: Medium Junction": {
+        "code":"autocmj",
+        "title": "Cones: Medium Junction",
+        "type":"counter"
+      },
+      "Cones: High Junction": {
+        "code":"autochj",
+        "title": "Cones: High Junction",
+        "type":"counter"
+},
+      "Parking Status": {
+        "code":"autoparkstat",
+        "title": "Parking Status",
+        "type":"radio",
+        "choices":{
+          "t":"Terminal<br>",
+          "s":"Substation<br>",
+          "x":"Not Attempted"
+        },
+        "defaultValue":"x"
+      },
+      "Signal Zone": {
+        "code":"autosignalz",
+        "title": "Signal Zone",
+        "type":"bool"
+      },
+      "Sleeve Zone": {
+        "code":"autosleevez",
+        "title": "Sleeve Zone",
+        "type":"bool"
+      }
+    },
+    "teleop": {
+      "Cones: In Terminal": {
+        "code":"cit",
+        "title": "Cones: In Terminal",
+        "type":"counter"
+      },
+      "Cones: Ground Junction": {
+        "code":"cgj",
+        "title": "Cones: Ground Junction",
+        "type":"counter"
+      },
+      "Cones: Low Junction": {
+        "code":"clj",
+        "title": "Cones: Low Junction",
+        "type":"counter"
+      },
+      "Cones: Medium Junction": {
+        "code":"cmj",
+        "title": "Cones: Medium Junction",
+        "type":"counter"
+      },
+      "Cones: High Junction": {
+        "code":"chj",
+        "title": "Cones: High Junction",
+        "type":"counter"
+      }
+},
+    "endgame": {
+      "Owned Poles": {
+        "code":"endowned",
+        "title": "# of Owned Cones",
+        "type":"counter"
+},
+      "Beacon Capped": {
+        "code":"endbeacon",
+        "title": "Sleeve Zone",
+        "type":"bool"
+},
+      "Circuit Completed": {
+        "code":"endcircuit",
+        "title": "Circuit Completed",
+        "type":"bool"
+      },
+      "Parking Status": {
+        "code":"endparkstat",
+        "title": "Parking Status",
+        "type":"radio",
+        "choices":{
+          "t":"Terminal<br>",
+          "s":"Substation<br>",
+          "x":"Not Attempted"
+        },
+        "defaultValue":"x"
+      }
+    },
+    "postmatch": {
+      "Driver Skill": {
+        "code":"ds",
+        "title": "Driver Skill",
+        "type":"radio",
+        "choices":{
+          "n":"Not Effective<br>",
+          "a":"Average<br>",
+          "v":"Very Effective<br>",
+          "x":"Not Observed"
+        },
+        "defaultValue":"x"
+      },
+       "Speed Rating": {
+        "code":"sr",
+        "title": "Speed Rating",
+        "type":"radio",
+        "choices":{
+          "1":"1 (slow)<br>",
+          "2":"2<br>",
+          "3":"3<br>",
+          "4":"4<br>",
+          "5":"5 (fast)"
+        },
+        "defaultValue":"3"
+      },
+      "Had Penalties": {
+        "code":"hp",
+        "title": "Had Penalties",
+        "type":"bool"
+      },
+      "Died/Tipped": {
+        "code":"d",
+        "title": "Died/Tipped",
+        "type":"bool"
+      },
+      "Make good alliance partner?": {
+        "code":"all",
+        "title": "Make good alliance partner?",
+        "type":"bool"
+      },
+      "Comments": {
+        "code":"co",
+        "title": "Comments",
+        "type":"text",
+        "size":15,
+        "maxSize":50
+      },
+      "Scouter's Confidence Rating <br>(of this data)": {
+        "code":"cnf",
+        "title": "Confidence Rating",
+        "type":"radio",
+        "choices":{
+          "v":"Very Confident<br>",
+          "a":"Average<br>",
+          "n":"Not Confident"
+      },
+       "defaultValue":"a"
+    }
+    }
+  }
+}`;
